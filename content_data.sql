5b8a026f4a0a215e9dd4657bdeb7bf52914af61f
Mr <37304121+sbondo1234@users.noreply.github.com>
2020-05-20 19:23:16 +0100

    add sql file

---
+++ content_data.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+-- Host:                         192.168.0.10
+-- Server version:               8.0.17 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table content_data.info: ~0 rows (approximately)
+/*!40000 ALTER TABLE `info` DISABLE KEYS */;
+INSERT INTO `info` (`id`, `Name`, `Category`, `Show_Type`, `Rating`, `Review`, `Year`, `Time_Added`, `Time_Updated`) VALUES
+	(1, 'Blackadder', 'Comedy', 'TV Show', '5/5', 's', 's', '2019-07-22 18:57:54', '2019-07-22 18:58:07');
+/*!40000 ALTER TABLE `info` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
